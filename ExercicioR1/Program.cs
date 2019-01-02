d93c355cc6555097f12a3baf6da3a1f307b71469
caiovicto546 <caiovicto@gmail.com>
2019-01-02 18:29:16 -0200

    Alteração do program.cs com a interação com o usuario

--- ExercicioR1/Program.cs
+++ ExercicioR1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using ExercicioR1.Entities;
+using ExercicioR1.Entities.Enums;
 
 namespace ExercicioR1
 {
@@ -7,23 +9,30 @@ namespace ExercicioR1
     {
         static void Main(string[] args)
         {
+            Department dept;
+            Worker worker;
+            WorkerLevel level;
             string department;
             string nome;
-            string level;
+            string monthAndYear;            
             double salary;
             int qtdContracts;
+            int month;
+            int year;
             
 
             Console.Write("Enter department's name:");
             department = Console.ReadLine();
+            dept = new Department(department);
 
             Console.WriteLine("Enter Worker data:");
             Console.Write("Name: ");
             nome = Console.ReadLine();
             Console.Write("Level (Junior/MidLevel/Senior): ");
-            level = Console.ReadLine();
+            level = Enum.Parse<WorkerLevel>(Console.ReadLine());
             Console.Write("Base Salary: R$");
             salary = double.Parse(Console.ReadLine());
+            worker = new Worker(nome, level, salary, dept);
 
             Console.Write("How many contracts to this worker?");
             qtdContracts = int.Parse(Console.ReadLine());
@@ -31,10 +40,11 @@ namespace ExercicioR1
            for (int i = 0; i<qtdContracts; i++)
             {
                 DateTime date;
+                HourContract contract;
                 double vph;
                 int duration;
 
-                Console.WriteLine("Enter #" + i + 1 + " Contract data:");
+                Console.WriteLine("Enter #" + (i + 1) + " Contract data:");
                 Console.Write("Data (DD/MM/YYYY): ");
                 date = DateTime.Parse(Console.ReadLine());
                 Console.Write("Value per hour: ");
@@ -42,7 +52,24 @@ namespace ExercicioR1
                 Console.Write("Duration (Hours): ");
                 duration = int.Parse(Console.ReadLine());
 
+                contract = new HourContract(date, vph, duration);
+                worker.AddContract(contract);
+
+                Console.WriteLine();
+
             }
+            Console.WriteLine();
+
+            Console.WriteLine("Enter mounth and year to calculate income (MM/YYYY): ");
+            monthAndYear = Console.ReadLine();
+
+            month = int.Parse(monthAndYear.Substring(0, 2));
+            year = int.Parse(monthAndYear.Substring(3));
+
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine("Income for " + monthAndYear + ": R$" + worker.Income(year,month),"F2",CultureInfo.InvariantCulture);
+
             Console.ReadLine();
         }
     }
